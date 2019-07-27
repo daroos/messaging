11476dc3bcf02d25d5a3e751cb84f1466870b65f
Dariusz Osiadacz <darekosiadacz@gmail.com>
2019-07-27 18:34:57 +0200

    Add bubble message when there's no internet

--- components/Status.js
+++ components/Status.js
@@ -30,13 +30,26 @@ export default class Status extends React.Component {
       />
     );
 
+    const messageContainer = (
+      <View style={styles.messageContainer} pointerEvents={"none"}>
+        {statusBar}
+        {!isConnected && (
+          <View style={styles.bubble}>
+            <Text style={styles.text}>No network connection</Text>
+          </View>
+        )}
+      </View>
+    );
+
     if (Platform.OS === "ios") {
       return (
-        <View style={[styles.status, { backgroundColor }]}>{statusBar}</View>
+        <View style={[styles.status, { backgroundColor }]}>
+          {messageContainer}
+        </View>
       );
     }
 
-    return null;
+    return messageContainer;
   }
 }
 
@@ -44,5 +57,23 @@ const styles = StyleSheet.create({
   status: {
     zIndex: 1,
     height: statusHeight
+  },
+  messageContainer: {
+    zIndex: 1,
+    position: "absolute",
+    top: statusHeight + 20,
+    right: 0,
+    left: 0,
+    height: 80,
+    alignItems: "center"
+  },
+  bubble: {
+    paddingHorizontal: 20,
+    paddingVertical: 10,
+    borderRadius: 20,
+    backgroundColor: "red"
+  },
+  text: {
+    color: "white"
   }
 });
